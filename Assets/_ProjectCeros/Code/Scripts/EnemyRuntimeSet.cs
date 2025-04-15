d2607d0d2f5bd7669a94fbd0e1734ed858f8c664
GrikerMan <130753542+GrikerMan@users.noreply.github.com>
2025-04-15 13:52:37 +0200

    New ordner structure and scene hierarchy structure.

--- Assets/_ProjectCeros/Code/Scripts/EnemyRuntimeSet.cs
+++
@@ -1,8 +0,0 @@
-using ProjectCeros;
-using UnityEngine;
-
-namespace ProjectCeros
-{
-    [CreateAssetMenu(menuName = "Runtime Sets/EnemyRuntimeSet")]
-    public class EnemyRuntimeSet : RuntimeSet<Enemy> { }
-}
