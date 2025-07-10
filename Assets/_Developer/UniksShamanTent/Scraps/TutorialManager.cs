b26287a5ac420ccdae96ab9dc67027b508961767
Jakobusy <136009044+Jakobusy@users.noreply.github.com>
2025-07-10 18:31:37 +0200

    feat(tutorial): added tutorial system base

---
+++ Assets/_Developer/UniksShamanTent/Scraps/TutorialManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace ProjectCeros
+{
+    public class TutorialManager : MonoBehaviour
+    {
+        [SerializeField] private BoolReference _wantsTutorial;
+        [SerializeField] private BoolReference _showedOnce;
+
+        [SerializeField] private TextMeshProUGUI _headingBox;
+        [SerializeField] private TextMeshProUGUI _textBox;
+
+        [SerializeField] private GameObject _uiElement;
+
+        [SerializeField] private string _heading;
+
+        [TextArea(3, 10)]
+        [SerializeField] private string _text;
+
+
+        public void FirstTutioralPrompt()
+        {
+            _headingBox.text = _heading;
+            _textBox.text = _text;
+            _uiElement.SetActive(true);
+        }
+
+
+        public void ShowClue()
+        {
+            if (_wantsTutorial && !_showedOnce)
+            {
+                _headingBox.text = _heading;
+                _textBox.text = _text;
+                _uiElement.SetActive(true);
+            }
+
+        }
+
+        public void WantsTutorial()
+        {
+            _wantsTutorial.Variable.SetValue(true);
+            _uiElement.SetActive(false);
+        }
+
+        public void WantsNoTutorial()
+        {
+            _wantsTutorial.Variable.SetValue(false);
+            _uiElement.SetActive(false);
+        }
+
+
+        public void ClueRead()
+        {
+            _uiElement.SetActive(false);
+            _showedOnce.Variable.SetValue(true);
+        }
+
+
+    }
+
+}
