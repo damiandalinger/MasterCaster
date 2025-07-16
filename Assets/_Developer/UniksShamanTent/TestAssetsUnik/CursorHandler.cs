6ce0efd79cfcd04ec7b93aa090d59648ef370177
Jakobusy <136009044+Jakobusy@users.noreply.github.com>
2025-07-16 13:10:37 +0200

    feat(system): minor adjustments

---
+++ Assets/_Developer/UniksShamanTent/TestAssetsUnik/CursorHandler.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace ProjectCeros
+{
+
+    public class CursorManager : MonoBehaviour
+    {
+        public Texture2D cursorTexture;
+        public Vector2 hotspot = Vector2.zero;
+        public CursorMode cursorMode = CursorMode.Auto;
+
+        void Start()
+        {
+            Cursor.SetCursor(cursorTexture, hotspot, cursorMode);
+        }
+    }
+}
