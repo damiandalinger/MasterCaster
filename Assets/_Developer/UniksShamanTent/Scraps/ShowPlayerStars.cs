26e63c4a2f1a09fa0a4e86c78bfeb992184f05a3
Jakobusy <136009044+Jakobusy@users.noreply.github.com>
2025-07-17 11:39:29 +0200

    feat(HUD): fixed star rating being an update

--- Assets/_Developer/UniksShamanTent/Scraps/ShowPlayerStars.cs
+++ Assets/_Developer/UniksShamanTent/Scraps/ShowPlayerStars.cs
@@ -1,34 +1,51 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace ProjectCeros
 {
-    public class ShowPlayerStats : MonoBehaviour
+       public class ShowPlayerStats : MonoBehaviour
     {
-
         public Transform starParent;
-
-        public Sprite filledStar;
-        public Sprite emptyStar;
 
         public IntReference Stars;
 
+        public float fillSpeed = 2f; // How fast each star fills (units per second)
 
-        public void Update()
+        public void ShowStarRating()
         {
-            ShowStarRating(Stars.Value);
+            StopAllCoroutines();
+            StartCoroutine(FillStarsSmoothly());
         }
 
-        public void ShowStarRating(int rating)
+        private IEnumerator FillStarsSmoothly()
         {
+            int targetStars = Mathf.Clamp(Stars, 0, starParent.childCount);
+
+            // Reset all stars to empty
             for (int i = 0; i < starParent.childCount; i++)
             {
                 var image = starParent.GetChild(i).GetComponent<Image>();
-                image.sprite = (i < rating) ? filledStar : emptyStar;
+                image.type = Image.Type.Filled;
+                image.fillAmount = 0f;
+            }
+
+            // Smoothly fill each star
+            for (int i = 0; i < targetStars; i++)
+            {
+                var image = starParent.GetChild(i).GetComponent<Image>();
+                image.type = Image.Type.Filled;
+
+                while (image.fillAmount < 1f)
+                {
+                    image.fillAmount += Time.deltaTime * fillSpeed;
+                    yield return null;
+                }
+
+                image.fillAmount = 1f;
             }
         }
-
-      }
+    }
 
 
 
